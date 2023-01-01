311838b00a0bf60b2c3e3bd58fa552cc9699ee06
Lavanya Narayanan <lavanya@diamanteblockchain.com>
2023-01-02 00:07:29 +0530

    added video player

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,10 +1,31 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-
+import Video from 'react-native-video';
+import video from './assets/video.mp4';
+import Video1 from './video.mp4';
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Video
+      source={Video1}
+      // ref={ref => {
+      //   this.player = ref;
+      // }}
+      // onBuffer={this.onBuffer}
+      // onError={this.videoError}
+      paused={false}
+      style={styles.backgroundVideo}
+      controls={true}
+      repeat={true}
+    />
   );
 }
+var styles = StyleSheet.create({
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    //margin: 10,
+  },
+});
